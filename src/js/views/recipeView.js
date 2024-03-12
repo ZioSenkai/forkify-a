b0e65e5a9389ccafc3a76e9050256cd95dda0b0b
ZioSenkai <thanhzi0610@gmail.com>
2024-03-12 16:20:54 +0700

    Remove the npm fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 
 // import icons from '../img/icons.svg'; // Parcel v1
 import icons from 'url:../../img/icons.svg'; // Parcel v2
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
